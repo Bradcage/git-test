c79a6c43bc3a67f4355cb90428ebd0fd0b8656f6
zitong Wen <zwen@ptc.com>
2022-08-12 09:22:57 +0800

    second commit test

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -2,5 +2,6 @@ public class HelloMaven {
     public static void main(String[] args) {
         System.out.println("hello maven");
         System.out.println("it is a change test!");
+        System.out.println("123");
     }
 }
